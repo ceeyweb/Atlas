7a710ee8f322f8b18df1bef2a4462ab01839be79
Gerardo Gaona <gerardo.gaona@gmail.com>
2020-03-06 12:29:30 -0600

    Add layout sidebar

---
+++ app/controllers/mobility_controller.rb
@@ -0,0 +1,7 @@
+class MobilityController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "mobility#index"
+
+  get "movilidad_social", to: "mobility#index", as: "social_mobility"
 end
