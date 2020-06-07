1c84c6b838b7b90d03059ddd64e4ca8c43273b39
jjmedinas <juan.ms@outlook.com>
2020-06-07 16:53:15 -0500

    Add section with buttons for mobility indicators

--- app/controllers/upward_mobility_indicator_controller.rb
+++ app/controllers/upward_mobility_indicator_controller.rb
@@ -1,6 +1,10 @@
 class UpwardMobilityIndicatorController < ApplicationController
   def show
     @data = UpwardMobilityIndicator.where_indicator(slug: mobility_params).scatter_data
+
+    if request.xhr?
+      return render json: @data
+    end
   end
 
   private
