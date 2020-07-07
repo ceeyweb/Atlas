34c342316336e9547ba1d4de5fa566572520ea09
Gerardo Gaona <gerardo.gaona@gmail.com>
2020-07-07 12:23:55 -0500

    Remove 'absoluta' from 'movilidad social ascendente'

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root "static_pages#introduction"
 
   resources :mobility, path: "movilidad_social", param: :slug, only: %i[index show]
-  resources :upward_mobility_indicator, path: "movilidad_ascendente_absoluta", param: :slug, only: %i[index show]
+  resources :upward_mobility_indicator, path: "movilidad_social_ascendente", param: :slug, only: %i[index show]
 end
 # rubocop:enable Layout/LineLength
